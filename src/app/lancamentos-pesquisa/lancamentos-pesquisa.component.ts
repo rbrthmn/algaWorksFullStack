eaa284a3ddb8d4e2d5e4cb424021f79936c99f16
Roberto <robertokenzohamano@gmail.com>
2020-08-12 16:42:29 -0300

    Adding pipes

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -8,20 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class LancamentosPesquisaComponent {
 
   lancamentos = [
-    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
-      dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José' },
-    { tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '10/06/2017',
-      dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Atacado Brasil' },
-    { tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: '20/07/2017',
-      dataPagamento: null, valor: 14312, pessoa: 'Ministério da Fazenda' },
-    { tipo: 'DESPESA', descricao: 'Mensalidade de escola', dataVencimento: '05/06/2017',
-      dataPagamento: '30/05/2017', valor: 800, pessoa: 'Escola Abelha Rainha' },
-    { tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: '18/08/2017',
-      dataPagamento: null, valor: 55000, pessoa: 'Sebastião Souza' },
+    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(30, 6, 2017),
+      dataPagamento: new Date(), valor: 4.55, pessoa: 'Padaria do José' },
+    { tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: new Date(10, 6, 2017),
+      dataPagamento: new Date(9, 6, 2017), valor: 80000, pessoa: 'Atacado Brasil' },
+    { tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: new Date(20, 7, 2017),
+      dataPagamento: new Date(), valor: 14312, pessoa: 'Ministério da Fazenda' },
+    { tipo: 'DESPESA', descricao: 'Mensalidade de escola', dataVencimento: new Date(5, 6, 2017),
+      dataPagamento: new Date(30, 5, 2017), valor: 800, pessoa: 'Escola Abelha Rainha' },
+    { tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: new Date(18, 8, 2017),
+      dataPagamento: new Date(), valor: 55000, pessoa: 'Sebastião Souza' },
     { tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: '10/07/2017',
-      dataPagamento: '09/07/2017', valor: 1750, pessoa: 'Casa Nova Imóveis' },
-    { tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: '13/07/2017',
-      dataPagamento: null, valor: 180, pessoa: 'Academia Top' }
+      dataPagamento: new Date(9, 7, 2017), valor: 1750, pessoa: 'Casa Nova Imóveis' },
+    { tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: new Date(13, 7, 2017),
+      dataPagamento: new Date(), valor: 180, pessoa: 'Academia Top' }
   ];
 
 }
